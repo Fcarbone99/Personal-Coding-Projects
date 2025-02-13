9558f9ceb0206b53fbee1831b08e6da17cfd97ca
Frankie Carbone <130788046+Fcarbone99@users.noreply.github.com>
2025-02-13 12:42:41 -0500

    Delete main.php

--- main.php
+++
@@ -1,8 +0,0 @@
-<div class="container mt-3">
-    <h2>Main Content</h2>
-    <div class="card">
-        <div class="card-body">
-            <p>This is the main content area. It adjusts based on the device size.</p>
-        </div>
-    </div>
-</div>
